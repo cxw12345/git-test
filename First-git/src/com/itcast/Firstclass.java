7d87f016daee4c10eee79e34fbfc9c55bd159a50
zhangsan <zhangsan@itcast.cn>
2019-05-03 17:10:56 +0800

    本地工程添加到git

--- First-git/src/com/itcast/Firstclass.java
+++ First-git/src/com/itcast/Firstclass.java
@@ -3,5 +3,7 @@ package com.itcast;
 public class Firstclass {
     public static void main(String[] args) {
         System.out.println("git------------------");
+        System.out.println("======================");
     }
+
 }
